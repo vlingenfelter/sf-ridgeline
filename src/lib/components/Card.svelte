823c7e7f5c35a17b286e358925d385a03d87f238
Violet Lingenfelter <vlingenfelter5@gmail.com>
2026-08-02 14:15:17 -0700

    add fonts inter and space grotesk via fontsource (#1)

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col">
-    <div class="flex-1 border-dotted bg-card-bg m-6 rounded-lg dark:bg-dark-card-bg border border-border dark:border-dark-border border-2">
+    <div class="flex-1 bg-card-bg m-6 rounded-lg dark:bg-dark-card-bg border border-border dark:border-dark-border border-1">
         <div class="p-6">
             {@render children?.()}
         </div>
