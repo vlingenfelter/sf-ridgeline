6e032f414538e4d9e807096ccf8a5375f0034808
Violet Lingenfelter <vlingenfelter5@gmail.com>
2026-08-02 18:45:52 -0700

    try AGAIN! for gh pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Card } from '$lib';
+</script>
+
+<svelte:head>
+  <title>Page not found</title>
+</svelte:head>
+
+<Card>
+    <h1>404! Page not found!</h1>
+    
+    <p>Sorry, this page doesn't exist.
+    <a href="/">Go home</a>
+    </p>
+
+</Card>
